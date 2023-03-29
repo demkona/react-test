a5f56ae7810c3d5647407c0eab190df9eea9b874
demkona <demkona@gmail.com>
2023-03-29 19:48:43 +0300

    add gh pages

--- src/hooks/usePosts.js
+++ src/hooks/usePosts.js
@@ -10,7 +10,6 @@ export const useSortedPosts = (posts, sort) => {
 
     return sortedPosts;
 }
-
 export const usePosts = (posts, sort, query) => {
     const sortedPosts = useSortedPosts(posts, sort);
 
